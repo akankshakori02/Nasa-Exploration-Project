e8482fe0ae001acbcde0efeb61b1270783e72606
akankshakori02 <akankshakori757@gmail.com>
2025-02-21 13:56:02 +0000

    Update server.js
    Added Frontend cors origin

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Enable CORS and JSON parsing for incoming requests
-app.use(cors());
+app.use(cors({ origin: "https://nasa-exploration-project-frontend.vercel.app" }));
 app.use(express.json());
 
 app.get('/', async (req, res) => {
